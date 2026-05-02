59a9ba1522f7fa32d610c59f3e79743ad5adc290
Paulo Heleno Dos Santos Pereira <helenop16@gmail.com>
2026-05-01 21:41:49 -0300

    Teste implementações

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const getMovimentos = require("./js/movimento");
-const { gerarTabuleiroComCaminho, imprimirTabuleiro } = require("./js/tabuleiro");
+const No = require("./js/no");
+const heuristicaForte = require("./heuristicas/heuristicaForte");
+const heuristicaFraca = require("./heuristicas/heuristicaFraca");
+const buscaGulosa = require("./buscas/gulosa");
+const aEstrela = require("./buscas/aestrela");
+const {
+  gerarTabuleiroAleatorio,
+  imprimirTabuleiro,
+  converterParaXadrez
+} = require("./js/tabuleiro");
+
+
 
 // 1. Gera o cenário
-const resultado = gerarTabuleiroComCaminho();
+const resultado = gerarTabuleiroAleatorio();
 
 const matriz = resultado.matriz;
 const origem = resultado.origem;
@@ -20,8 +31,56 @@ const movimentos = getMovimentos(matriz, casaInicial);
 // 5. Mostra resultados
 console.log("\nMovimentos possíveis da torre:");
 
+const hForte = heuristicaForte(casaInicial, destino);
+const hFraca = heuristicaFraca(casaInicial, destino);
+
+const noInicial = new No(casaInicial);
+
+console.log("\nHeurísticas:");
+console.log("Forte:", hForte);
+console.log("Fraca:", hFraca);
+
+console.log("\nNó inicial:");
+console.log({
+  posicao: converterParaXadrez(casaInicial.linha, casaInicial.coluna),
+  g: noInicial.g,
+  h: noInicial.h,
+  f: noInicial.f
+});
+
+const caminho = buscaGulosa(matriz, origem, destino, heuristicaForte);
+
+console.log("\nCaminho encontrado (Gulosa):");
+
+if (caminho) {
+  caminho.forEach(c => {
+    const pos = converterParaXadrez(c.linha, c.coluna);
+    console.log(pos);
+  });
+} else {
+  console.log("Nenhum caminho encontrado");
+}
+
+const resultadoA = aEstrela(matriz, origem, destino, heuristicaForte);
+
+console.log("\nCaminho encontrado (A*):");
+
+if (resultadoA) {
+  resultadoA.caminho.forEach(c => {
+    const pos = converterParaXadrez(c.linha, c.coluna);
+    console.log(pos);
+  });
+
+  console.log("\nNós expandidos:", resultadoA.nosExpandidos.size);
+} else {
+  console.log("Nenhum caminho encontrado");
+}
+
 movimentos.forEach(m => {
+  const pos = converterParaXadrez(m.casa.linha, m.casa.coluna);
+
   console.log(
-    `${m.direcao} → (${m.casa.linha}, ${m.casa.coluna}) | custo: ${m.casa.custo}`
+    `${m.direcao} → ${pos} | custo: ${m.casa.custo}`
   );
 });
+
